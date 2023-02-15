f2c1018fc33cdb02617bbd7070c8b39856877ada
ReigoRaudsepp <115535182+ReigoRaudsepp@users.noreply.github.com>
2023-02-15 09:30:30 +0200

    Update ul1.py

--- ul1.py
+++ ul1.py
@@ -1,16 +1,15 @@
 with open('sammud.txt', 'r') as f:
-    sammud = [(line) for line in f]
+    sammud = [int(line) for line in f]
 
 kokku = sum(sammud)
 print(f"Nädala kokku sammud: {kokku}")
 
-keskmised = [sammud[i:i+1] for i in range(0, len(sammud), 1)]
-keskmised = [sum(p)/len(p) for p in keskmised]
+keskmised = [sum(sammud[i:i+1])/1 for i in range(len(sammud))]
 print(f"Päeva keskmised sammud: {keskmised}")
 
 suurim = max(sammud)
-suurima_indeks = sammud.index(suurim)
+suurima_indeks = sammud.index(suurim) + 1
 vaikseim = min(sammud)
-vaikseima_indeks = sammud.index(vaikseim)
-print(f"Kõige suurem sammude arv oli {suurim} päeval {suurima_indeks+1}")
-print(f"Kõige väiksem sammude arv oli {vaikseim} päeval {vaikseima_indeks+1}")
+vaikseima_indeks = sammud.index(vaikseim) + 1
+print(f"Kõige suurem sammude arv oli {suurim} päeval {suurima_indeks}")
+print(f"Kõige väiksem sammude arv oli {vaikseim} päeval {vaikseima_indeks}")
